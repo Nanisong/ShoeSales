6a0605fb848d30d78d56d1637cdbed219e6f3dc1
LanGit <langnos@gmail.com>
2023-10-18 10:08:21 +0800

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoeSales.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public virtual List<Product> Products { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ShoeSales.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public string? Location { get; set; }
+        public Decimal Size { get; set; }
+        public Decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        [JsonIgnore]
+        public virtual Category? Category { get; set; }
+    }
+}
+
